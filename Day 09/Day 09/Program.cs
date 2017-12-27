2f36882a1bb6e40b7ba3295da98b4d387a8c9d14
ATKelman <a14aleke@student.his.se>
2017-12-27 23:04:31 +0100

    Added solution for Day 09

--- Day 09/Day 09/Program.cs
+++ Day 09/Day 09/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Day_09
 {
@@ -10,6 +7,68 @@ namespace Day_09
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(GetCount(true));
+            Console.WriteLine(GetCount(false));
+
+            Console.ReadKey();
+        }
+
+        static int GetCount(bool answer)
+        {
+            var input = File.ReadAllText("input.txt");
+
+            var groupCount = 0;
+            var score = 0;
+            var garbageCount = 0;
+
+            var isGarbage = false;
+            var shouldSkip = false;
+
+            foreach(char c in input)
+            {
+                if(shouldSkip)
+                {
+                    shouldSkip = false;
+                    continue;
+                }
+                if(c == '!')
+                {
+                    shouldSkip = true;
+                    continue;
+                }
+
+                if(c == '<')
+                {
+                    if(!isGarbage)
+                    {
+                        isGarbage = true;
+                        continue;
+                    }
+                }
+                else if(c== '>')
+                {
+                    isGarbage = false;
+                    continue;
+                }
+
+                if(c == '{' && !isGarbage)
+                {
+                    groupCount++;
+                }
+                if(c =='}' && !isGarbage)
+                {
+                    score += groupCount;
+                    groupCount--;
+                }
+
+                if(isGarbage)
+                    garbageCount++;
+            }
+
+            if (answer)
+                return score;
+            else
+                return garbageCount;
         }
     }
 }
