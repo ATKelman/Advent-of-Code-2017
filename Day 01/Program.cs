34a396147e60c34dba54af9bba988ff928c2e83d
Alexander Kelman <alexander.kelman@cgi.com>
2017-12-29 09:17:35 +0100

    Added Day 01 & 02 code

--- Day 01/Program.cs
+++ Day 01/Program.cs
@@ -8,8 +8,49 @@ namespace Day_01
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-        }
-    }
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Advent of Code Day 01");
+			var result = GetSumHalfStep("12131415");
+			Console.WriteLine(result);
+			Console.ReadKey();
+		}
+
+		static int GetSum(string input)
+		{
+			var sum = 0;
+			var previous = -1;
+			for (int i = 0; i < input.Length + 1; i++)
+			{
+				var counter = i % input.Length;
+				var instance = (int)Char.GetNumericValue(input[counter]);
+				if (instance == previous)
+				{
+					sum += instance;
+				}
+				previous = instance;
+			}
+
+			return sum;
+		}
+
+		static int GetSumHalfStep(string input)
+		{
+			var sum = 0;
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				var comparison = (i + input.Length / 2) % input.Length;
+
+				var valueCurrent = (int)Char.GetNumericValue(input[i]);
+				var valueComparison = (int)Char.GetNumericValue(input[comparison]);
+				if (valueCurrent == valueComparison)
+				{
+					sum += valueCurrent;
+				}
+			}
+
+			return sum;
+		}
+	}
 }
