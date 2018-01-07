3520a5b7bdf3b27e60c551b25c62a5e5a56844f3
ATKelman <a14aleke@student.his.se>
2018-01-07 21:02:30 +0100

    Day 13 Part 1 & 2

---
+++ Day 13/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PartOne());
+            Console.WriteLine(PartTwo());
+
+            Console.ReadKey();
+        }
+
+        static int PartOne()
+        {
+            var input = File.ReadAllLines("input.txt").Select(x => new Security(x)).ToList();
+            var severity = 0;
+
+            foreach (var current in input)
+            {
+                if (current.IsCaught(0))
+                    severity += current.depth * current.range;
+            }
+
+            return severity;
+        }
+
+        static int PartTwo()
+        {
+            var input = File.ReadAllLines("input.txt").Select(x => new Security(x)).ToList();
+            var delays = 0;
+            var caught = true;
+
+            while(caught)
+            {
+                caught = false;
+                foreach (var current in input)
+                {
+                    if (current.IsCaught(delays))
+                    {
+                        caught = true;
+                        break;
+                    }                       
+                }
+                if(caught)
+                    delays++;
+            }
+
+            return delays;
+        }
+        
+        public class Security
+        {
+            public int depth;
+            public int range;
+
+            public Security(string input)
+            {
+                var words = input.Split(new string[] { ": " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                depth = int.Parse(words[0]);
+                range = int.Parse(words[1]);
+            }
+
+            public bool IsCaught(int delay)
+            {
+                return ((depth + delay) % ((range - 1) * 2) == 0);
+            }           
+        }
+    }
+}
